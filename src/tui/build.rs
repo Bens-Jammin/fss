64e849416f8a8298662655eb14519f7822c5473c
BenisJammin <benemiller994@gmail.com>
2026-07-23 15:48:15 -0400

    rename cli dir to tui

--- src/tui/build.rs
+++ src/tui/build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 
 fn main() {
     let manifest_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
-    // src/cli -> up two levels -> repo root, where libfssbackend.a lives
+    // src/tui -> up two levels -> repo root, where libfssbackend.a lives
     let backend_dir = manifest_dir.join("../../build").canonicalize().unwrap();
 
     println!("cargo:rustc-link-search=native={}", backend_dir.display());
